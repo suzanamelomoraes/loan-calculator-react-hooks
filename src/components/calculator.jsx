c9bdcd57ef6d50dd250cf0d542a23d96d8c5083c
Suzana-Melo-Moraes <suzanamelojor@gmail.com>
2020-07-20 17:27:14 +1200

    create a file to add the components and include a component called calculator which has states for amount, interest and year

---
+++ src/components/calculator.jsx
@@ -0,0 +1,10 @@
+import React, { useState } from 'react';
+
+function Calculator() {
+  const [amount, setAmount] = useState('');
+  const [interest, setInterest] = useState('');
+  const [year, setYear] = useState('');
+  return <div></div>;
+}
+
+export default Calculator;
