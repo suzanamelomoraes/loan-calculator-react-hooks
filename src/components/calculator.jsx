9c0ec8d741262a988d0190f51f8fa1ecd1fe01bc
Suzana-Melo-Moraes <suzanamelojor@gmail.com>
2020-07-20 17:45:38 +1200

    create 3 input fields to receive amount, interest and year values amd set these values

--- src/components/calculator.jsx
+++ src/components/calculator.jsx
@@ -4,7 +4,30 @@ function Calculator() {
   const [amount, setAmount] = useState('');
   const [interest, setInterest] = useState('');
   const [year, setYear] = useState('');
-  return <div></div>;
+  return (
+    <div>
+      <form>
+        <input
+          type='number'
+          value={amount}
+          placeholder='amount'
+          onChange={(e) => setAmount(e.target.amount)}
+        />
+        <input
+          type='number'
+          value={interest}
+          placeholder='interest'
+          onChange={(e) => setInterest(e.target.interest)}
+        />
+        <input
+          type='number'
+          value={year}
+          placeholder='year'
+          onChange={(e) => setYear(e.target.year)}
+        />
+      </form>
+    </div>
+  );
 }
 
 export default Calculator;
