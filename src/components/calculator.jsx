5eadc66b2506b405156e5610171cb43616730ce7
Suzana-Melo-Moraes <suzanamelojor@gmail.com>
2020-07-20 18:32:47 +1200

    create a button that listen for a click and call handleSubmit function

--- src/components/calculator.jsx
+++ src/components/calculator.jsx
@@ -4,6 +4,16 @@ function Calculator() {
   const [amount, setAmount] = useState('');
   const [interest, setInterest] = useState('');
   const [year, setYear] = useState('');
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    if (!amount || !interest || !year) return;
+  };
+
+  console.log(amount);
+  console.log(interest);
+  console.log(year);
+
   return (
     <div>
       <form>
@@ -11,21 +21,22 @@ function Calculator() {
           type='number'
           value={amount}
           placeholder='amount'
-          onChange={(e) => setAmount(e.target.amount)}
+          onChange={(e) => setAmount(e.target.value)}
         />
         <input
           type='number'
           value={interest}
           placeholder='interest'
-          onChange={(e) => setInterest(e.target.interest)}
+          onChange={(e) => setInterest(e.target.value)}
         />
         <input
           type='number'
           value={year}
           placeholder='year'
-          onChange={(e) => setYear(e.target.year)}
+          onChange={(e) => setYear(e.target.value)}
         />
       </form>
+      <button onClick={() => handleSubmit()}>Submit</button>
     </div>
   );
 }
