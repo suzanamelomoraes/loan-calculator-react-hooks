cc006e051c5632e9e4911287cfbd4b019863e583
Suzana-Melo-Moraes <suzanamelojor@gmail.com>
2020-07-23 12:32:33 +1200

    update from to the hook-form model giving the key names as amount, interest and year to collect the data and register it. Also write onSubmit function to access the data

--- src/components/calculator.jsx
+++ src/components/calculator.jsx
@@ -2,79 +2,98 @@ import React, { useState } from 'react';
 import { useForm } from 'react-hook-form';
 
 function Calculator() {
-  const [amount, setAmount] = useState('');
-  const [interest, setInterest] = useState('');
-  const [year, setYear] = useState('');
-  const [monthlyPayment, setMonthlyPayment] = useState('');
-  const [totalPayment, setTotalPayment] = useState('');
-  const [totalInterest, setTotalInterest] = useState('');
+    const {register, handleSubmit} = useForm();
 
-  const handleSubmit = (e) => {
-    e.preventDefault();
-    if (!amount || !interest || !year) return;
-    calculateResults(amount, interest, year);
-    setAmount('');
-    setInterest('');
-    setYear('');
-  };
+    const onSubmit = data => console.log(data);
 
-  const calculateResults = (amount, interest, year) => {
-    const initialAmount = amount,
-      calculatedinterest = interest / 100 / 12,
-      calculatedPayments = year * 12,
-      x = Math.pow(1 + calculatedinterest, calculatedPayments),
-      monthly = (initialAmount * x * calculatedinterest) / (x - 1);
+//   const [amount, setAmount] = useState('');
+//   const [interest, setInterest] = useState('');
+//   const [year, setYear] = useState('');
+//   const [monthlyPayment, setMonthlyPayment] = useState('');
+//   const [totalPayment, setTotalPayment] = useState('');
+//   const [totalInterest, setTotalInterest] = useState('');
 
-    if (isFinite(monthly)) {
-      const initialMonthlyPayment = monthly.toFixed(2),
-        initialTotalPayment = (monthly * calculatedPayments).toFixed(2),
-        initialTotalInterest = (
-          monthly * calculatedPayments -
-          initialAmount
-        ).toFixed(2);
-      setMonthlyPayment(initialMonthlyPayment);
-      setTotalPayment(initialTotalPayment);
-      setTotalInterest(initialTotalInterest);
-    }
-  };
+//   const handleSubmit = (e) => {
+//     e.preventDefault();
+//     if (!amount || !interest || !year) return;
+//     calculateResults(amount, interest, year);
+//     setAmount('');
+//     setInterest('');
+//     setYear('');
+//   };
+
+//   const calculateResults = (amount, interest, year) => {
+//     const initialAmount = amount,
+//       calculatedinterest = interest / 100 / 12,
+//       calculatedPayments = year * 12,
+//       x = Math.pow(1 + calculatedinterest, calculatedPayments),
+//       monthly = (initialAmount * x * calculatedinterest) / (x - 1);
+
+//     if (isFinite(monthly)) {
+//       const initialMonthlyPayment = monthly.toFixed(2),
+//         initialTotalPayment = (monthly * calculatedPayments).toFixed(2),
+//         initialTotalInterest = (
+//           monthly * calculatedPayments -
+//           initialAmount
+//         ).toFixed(2);
+//       setMonthlyPayment(initialMonthlyPayment);
+//       setTotalPayment(initialTotalPayment);
+//       setTotalInterest(initialTotalInterest);
+//     }
+//   };
 
   return (
-    <div>
-      <form className='myform' onSubmit={handleSubmit}>
-        <div className='form-items'>
-          <label>Amount:</label>
-          <input
-            type='number'
-            value={amount}
-            placeholder='Total amount'
-            onChange={(e) => setAmount(e.target.value)}
-          />
+      <div>
 
-          <label> Interest:</label>
-          <input
-            type='number'
-            value={interest}
-            placeholder='Interest'
-            onChange={(e) => setInterest(e.target.value)}
-          />
+      
+      <form className='myform' onSubmit={handleSubmit(onSubmit)} >
+          <div className='form-items'>
+          <input type="number" name='amount' ref={register} />
+          <input type="number"name='interest'ref={register}/>
+          <input type="number"name='year'ref={register}/>
+          <input type="submit" className='submit'/>
+          </div>
 
-          <label> Years:</label>
-          <input
-            type='number'
-            value={year}
-            placeholder='Years to repay'
-            onChange={(e) => setYear(e.target.value)}
-          />
-        </div>
-
-        <input type='submit' value='Submit' className='submit' />
       </form>
 
-      <div>
-        <p>Monthly Payment: {monthlyPayment}</p>
-        <p>Total Payment: {totalPayment}</p>
-        <p>Total Interest: {totalInterest}</p>
-      </div>
+    {/* // <div>
+    //   <form className='myform' onSubmit={handleSubmit}>
+    //     <div className='form-items'>
+    //       <label>Amount:</label>
+    //       <input */}
+    {/* //         type='number'
+    //         value={amount}
+    //         placeholder='Total amount'
+    //         onChange={(e) => setAmount(e.target.value)}
+    //       />
+
+    //       <label> Interest:</label>
+    //       <input */}
+    {/* //         type='number'
+    //         value={interest}
+    //         placeholder='Interest'
+    //         onChange={(e) => setInterest(e.target.value)}
+    //       />
+
+    //       <label> Years:</label>
+    //       <input */}
+    {/* //         type='number'
+    //         value={year}
+    //         placeholder='Years to repay'
+    //         onChange={(e) => setYear(e.target.value)}
+    //       />
+    //     </div> */}
+
+    {/* //     <input type='submit' value='Submit' className='submit' />
+    //   </form> */}
+
+    {/* //   <div>
+    //     <p>Monthly Payment: {monthlyPayment}</p>
+    //     <p>Total Payment: {totalPayment}</p>
+    //     <p>Total Interest: {totalInterest}</p>
+    //   </div>
+
+    </div> */}
     </div>
   );
 }
