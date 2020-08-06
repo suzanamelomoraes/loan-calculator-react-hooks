aec7f41e9e04bfad201720e572c1d508846b179c
Suzana-Melo-Moraes <suzanamelojor@gmail.com>
2020-08-06 15:11:27 +1200

    install mutationobserver to fix test issue

--- src/components/__test__/calculator.test.js
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import Calculator from './../calculator/calculator';
-
-it('renders without crashing', () => {
-  const div = document.createElement('div');
-  ReactDOM.render(<form></form>, div);
-});
