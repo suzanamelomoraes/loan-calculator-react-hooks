a5c79f41859eff5a68d7b1ac4321e2524ec2c936
Suzana-Melo-Moraes <suzanamelojor@gmail.com>
2020-08-06 13:34:27 +1200

    create a test to check if the calculator component is rendering without crashing

--- src/components/__test__/calculator.test.js
+++ src/components/__test__/calculator.test.js
@@ -1,3 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import Calculator from './../calculator/calculator';
+
+it('renders without crashing', () => {
+  const div = document.createElement('div');
+  ReactDOM.render(<form></form>, div);
+});
