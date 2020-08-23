9e40dcbd104cf07e0098af360c3ce4f72f578837
Suzana-Melo-Moraes <suzanamelojor@gmail.com>
2020-08-24 11:11:45 +1200

    create a state to store the values given by the user and create a form with inputs and labels for those values

--- src/components/calculator/Calculator.jsx
+++ src/components/calculator/Calculator.jsx
@@ -1,7 +1,30 @@
 import React, { useState } from 'react';
 
 function Calculator() {
-  return;
+  const [valuesGivenByUser, setValues] = useState({
+    amount: '',
+    interest: '',
+    years: '',
+  });
+
+  return (
+    <form className='myform'>
+      <div className='form-items'>
+        <div>
+          <label id='mylabel'>Amount:</label>
+          <input type='text' name='amount' placeholder='Loan amount' />
+        </div>
+        <div>
+          <label id='mylabel'>Interest:</label>
+          <input type='text' name='interest' placeholder='Interest' />
+        </div>
+        <div>
+          <label id='mylabel'>Years:</label>
+          <input type='text' name='years' placeholder='Years to repay' />
+        </div>
+      </div>
+    </form>
+  );
 }
 
 export default Calculator;
