669fdbe22b2ac4f100de612217b4bcffcabf7aa8
Suzana-Melo-Moraes <suzanamelojor@gmail.com>
2020-08-24 19:16:49 +1200

    create a state to store error messages, also create a function to validade the inputs values given by the user and handle to set the error messages, finally include a paragraph above the form to display the error message when it exists

--- src/components/calculator/Calculator.jsx
+++ src/components/calculator/Calculator.jsx
@@ -16,10 +16,45 @@ function Calculator() {
     isResult: false,
   });
 
+  const [error, setError] = useState('');
+
+  const validate = () => {
+    let actualError = '';
+    // Validate if there are values
+    if (!userValues.amount || !userValues.interest || !userValues.years) {
+      actualError = 'All the values are required';
+    }
+    // Validade if the values are numbers
+    if (
+      isNaN(userValues.amount) ||
+      isNaN(userValues.interest) ||
+      isNaN(userValues.years)
+    ) {
+      actualError = 'Please give a positive number';
+    }
+    // Validade if the values are positive numbers
+    if (
+      parseFloat(userValues.amount) <= 0 ||
+      parseFloat(userValues.interest) <= 0 ||
+      parseFloat(userValues.years) <= 0
+    ) {
+      actualError = 'Please give a positive number';
+    }
+    if (actualError) {
+      setError(actualError);
+      return false;
+    }
+    return true;
+  };
+
+  // Handle the data submited by the user by sending it to the calculateResults function
   const handleSubmitValues = (e) => {
     e.preventDefault();
-    console.log('user values', userValues);
-    calculateResults(userValues);
+    const isValid = validate();
+    if (isValid) {
+      setError('');
+      calculateResults(userValues);
+    }
   };
 
   // Calculation
@@ -35,15 +70,13 @@ function Calculator() {
         totalPayment = (monthly * calculatedPayments).toFixed(2),
         totalInterest = (monthly * calculatedPayments - userAmount).toFixed(2);
 
-      // Set up results to the state to display them to the user
+      // Set up results to the state to be displayed to the user
       const newResults = { ...results };
       newResults.monthlyPaymentUI = monthlyPayment;
       newResults.totalPaymentUI = totalPayment;
       newResults.totalInterestUI = totalInterest;
       newResults.isResult = true;
       setResults(newResults);
-      console.log('results after calculation', newResults);
-      console.log('results after set the state', results);
     }
     return;
   };
@@ -65,75 +98,82 @@ function Calculator() {
   };
 
   return (
-    <form className='myform' onSubmit={handleSubmitValues}>
-      {!results.isResult ? (
-        <div className='form-items'>
-          <div>
-            <label id='mylabel'>Amount:</label>
+    <div>
+      <p className='error'>{error}</p>
+      <form className='myform' onSubmit={handleSubmitValues}>
+        {/* ternary operator manages when the calculator and results will be displayed to the user */}
+        {!results.isResult ? (
+          //   Form to collect data from the user
+          <div className='form-items'>
+            <div>
+              <label id='mylabel'>Amount:</label>
+              <input
+                type='text'
+                name='amount'
+                placeholder='Loan amount'
+                value={userValues.amount}
+                //   onChange sets the values given by the user as input to the userValues state
+                onChange={(event) =>
+                  setUserValues({ ...userValues, amount: event.target.value })
+                }
+              />
+            </div>
+            <div>
+              <label id='mylabel'>Interest:</label>
+              <input
+                type='text'
+                name='interest'
+                placeholder='Interest'
+                value={userValues.interest}
+                onChange={(event) =>
+                  setUserValues({ ...userValues, interest: event.target.value })
+                }
+              />
+            </div>
+            <div>
+              <label id='mylabel'>Years:</label>
+              <input
+                type='text'
+                name='years'
+                placeholder='Years to repay'
+                value={userValues.years}
+                onChange={(event) =>
+                  setUserValues({ ...userValues, years: event.target.value })
+                }
+              />
+            </div>
+            <input type='submit' className='button' />
+          </div>
+        ) : (
+          //   Form to display the results to the user
+          <div className='form-items'>
+            <h4>
+              Loan amount: ${userValues.amount} <br /> Interest:{' '}
+              {userValues.interest}% <br /> Years to repay: {userValues.years}
+            </h4>
+            <div>
+              <label id='mylabel'>Monthly Payment:</label>
+              <input type='text' value={results.monthlyPaymentUI} disabled />
+            </div>
+            <div>
+              <label id='mylabel'>Total Payment: </label>
+              <input type='text' value={results.totalPaymentUI} disabled />
+            </div>
+            <div>
+              <label id='mylabel'>Total Interest:</label>
+              <input type='text' value={results.totalInterestUI} disabled />
+            </div>
+            {/* Button to clear fields */}
             <input
-              type='text'
-              name='amount'
-              placeholder='Loan amount'
-              value={userValues.amount}
-              onChange={(event) =>
-                setUserValues({ ...userValues, amount: event.target.value })
-              }
+              className='button'
+              value='Calculate again'
+              type='button'
+              onClick={() => clearFields()}
             />
           </div>
-          <div>
-            <label id='mylabel'>Interest:</label>
-            <input
-              type='text'
-              name='interest'
-              placeholder='Interest'
-              value={userValues.interest}
-              onChange={(event) =>
-                setUserValues({ ...userValues, interest: event.target.value })
-              }
-            />
-          </div>
-          <div>
-            <label id='mylabel'>Years:</label>
-            <input
-              type='text'
-              name='years'
-              placeholder='Years to repay'
-              value={userValues.years}
-              onChange={(event) =>
-                setUserValues({ ...userValues, years: event.target.value })
-              }
-            />
-          </div>
-          <input type='submit' className='button' />
-        </div>
-      ) : (
-        //   Display the results to the user
-        <div className='form-items'>
-          <h4>
-            Loan amount: ${userValues.amount} <br /> Interest:
-            {userValues.interest}% <br /> Years to repay: {userValues.years}
-          </h4>
-          <div>
-            <label id='mylabel'>Monthly Payment:</label>
-            <input type='text' value={results.monthlyPaymentUI} disabled />
-          </div>
-          <div>
-            <label id='mylabel'>Total Payment: </label>
-            <input type='text' value={results.totalPaymentUI} disabled />
-          </div>
-          <div>
-            <label id='mylabel'>Total Interest:</label>
-            <input type='text' value={results.totalInterestUI} disabled />
-          </div>
-          <input
-            className='button'
-            value='Calculate again'
-            type='button'
-            onClick={() => clearFields()}
-          />
-        </div>
-      )}
-    </form>
+        )}
+      </form>
+    </div>
   );
 }
 
