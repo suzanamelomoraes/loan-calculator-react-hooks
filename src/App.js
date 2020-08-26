2f73542822662b07de48f521a930fe91d4ce6a2c
Suzana-Melo-Moraes <suzanamelojor@gmail.com>
2020-08-26 21:28:39 +1200

    refactor classnames position

--- src/App.js
+++ src/App.js
@@ -1,16 +1,13 @@
 import React from 'react';
-// import Calculator from './components/calculator/Calculator';
-import CalculatorHookForm from './components/calculator/CalculatorHookForm';
+import Calculator from './components/calculator/Calculator';
+// import CalculatorHookForm from './components/calculator/CalculatorHookForm';
 import './App.css';
 
 function App() {
   return (
-    <div className='calculator'>
-      <div className='container'>
-        <h1>Loan Calculator</h1>
-        {/* <Calculator /> */}
-        <CalculatorHookForm />
-      </div>
+    <div>
+      <Calculator />
+      {/* <CalculatorHookForm /> */}
     </div>
   );
 }
